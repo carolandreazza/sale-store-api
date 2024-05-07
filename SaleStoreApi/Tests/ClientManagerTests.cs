4ea604d9008ca00e34e74c5ec0a3bec5295707c7
Caroline A. Bandeira <carol_bandeira7@hotmail.com>
2024-05-07 04:54:52 -0300

    Tests

---
+++ SaleStoreApi/Tests/ClientManagerTests.cs
@@ -0,0 +1,94 @@
+﻿using NUnit.Framework;
+using SaleStoreApi.Models;
+using SaleStoreApi.Repositories;
+using SaleStoreApi.Services;
+using System;
+
+//criado mas contém erros
+namespace SaleStoreApi.Tests
+{   
+    /*
+    [TestFixture]
+    public class ClientManagerTests
+    {
+        private ClientManager _clientManager;
+
+        [SetUp]
+        public void Setup()
+        {
+            var clientRepository = new ClientRepository();
+            _clientManager = new ClientManager(clientRepository);
+        }
+
+        [Test]
+        public void RegisterNewClient_ShouldRegisterClient()
+        {
+            // Arrange
+            var newClient = new Client("New Client", "New Address", "New Phone");
+
+            // Act
+            _clientManager.RegisterNewClient(newClient);
+
+            // Assert
+            var registeredClient = _clientManager.GetClientByName("New Client");
+            Assert.NotNull(registeredClient);
+            Assert.AreEqual("New Client", registeredClient.Name);
+            Assert.AreEqual("New Address", registeredClient.Address);
+            Assert.AreEqual("New Phone", registeredClient.Phone);
+        }
+
+        [Test]
+        public void GetAllClients_ShouldReturnAllClients()
+        {
+            // Arrange
+            var client1 = new Client("Client 1", "Address 1", "Phone 1");
+            var client2 = new Client("Client 2", "Address 2", "Phone 2");
+            var client3 = new Client("Client 3", "Address 3", "Phone 3");
+
+            _clientManager.RegisterNewClient(client1);
+            _clientManager.RegisterNewClient(client2);
+            _clientManager.RegisterNewClient(client3);
+
+            // Act
+            var allClients = _clientManager.GetAllClients();
+
+            // Assert
+            Assert.AreEqual(3, allClients.Count);
+            Assert.Contains(client1, allClients);
+            Assert.Contains(client2, allClients);
+            Assert.Contains(client3, allClients);
+        }
+
+        [Test]
+        public void UpdateClient_ShouldUpdateClient()
+        {
+            // Arrange
+            var existingClient = new Client("Existing Client", "Existing Address", "Existing Phone");
+            _clientManager.RegisterNewClient(existingClient);
+
+            // Act
+            _clientManager.UpdateClient("Existing Client", "Updated Address", "Updated Phone");
+
+            // Assert
+            var updatedClient = _clientManager.GetClientByName("Existing Client");
+            Assert.AreEqual("Updated Address", updatedClient.Address);
+            Assert.AreEqual("Updated Phone", updatedClient.Phone);
+        }
+
+        [Test]
+        public void DeleteClient_ShouldDeleteClient()
+        {
+            // Arrange
+            var existingClient = new Client("Client to Delete", "Address", "Phone");
+            _clientManager.RegisterNewClient(existingClient);
+
+            // Act
+            _clientManager.DeleteClient("Client to Delete");
+
+            // Assert
+            var deletedClient = _clientManager.GetClientByName("Client to Delete");
+            Assert.IsNull(deletedClient);
+        }
+    }
+    */
+}
---
+++ SaleStoreApi/Tests/ProductManagerTests.cs
@@ -0,0 +1,98 @@
+﻿using NUnit.Framework;
+using SaleStoreApi.Models;
+using SaleStoreApi.Repositories;
+using SaleStoreApi.Services;
+using System;
+
+//criado mas contém erros
+namespace SaleStoreApi.Tests
+{      
+    /*
+    [TestFixture]
+    public class ProductManagerTests
+    {
+        private ProductManager _productManager;
+
+        [SetUp]
+        public void Setup()
+        {
+            var productRepository = new ProductRepository();
+            _productManager = new ProductManager(productRepository);
+        }
+
+        [Test]
+        public void RegisterNewProduct_ShouldRegisterProduct()
+        {
+            // Arrange
+            var newProduct = new Product("New Product", 20.99m, "New Description", "New Supplier", DateTime.Now.AddDays(60));
+
+            // Act
+            _productManager.RegisterNewProduct(newProduct);
+
+            // Assert
+            var registeredProduct = _productManager.GetProductByName("New Product");
+            Assert.NotNull(registeredProduct);
+            Assert.AreEqual("New Product", registeredProduct.Name);
+            Assert.AreEqual(20.99m, registeredProduct.Price);
+            Assert.AreEqual("New Description", registeredProduct.Description);
+            Assert.AreEqual("New Supplier", registeredProduct.Supplier);
+            Assert.AreEqual(DateTime.Now.AddDays(60).Date, registeredProduct.ExpiryDate.Date);
+        }
+
+        [Test]
+        public void GetAllProducts_ShouldReturnAllProducts()
+        {
+            // Arrange
+            var product1 = new Product("Product 1", 15.99m, "Description 1", "Supplier 1", DateTime.Now.AddDays(30));
+            var product2 = new Product("Product 2", 25.99m, "Description 2", "Supplier 2", DateTime.Now.AddDays(60));
+            var product3 = new Product("Product 3", 35.99m, "Description 3", "Supplier 3", DateTime.Now.AddDays(90));
+
+            _productManager.RegisterNewProduct(product1);
+            _productManager.RegisterNewProduct(product2);
+            _productManager.RegisterNewProduct(product3);
+
+            // Act
+            var allProducts = _productManager.GetAllProducts();
+
+            // Assert
+            Assert.AreEqual(3, allProducts.Count);
+            Assert.Contains(product1, allProducts);
+            Assert.Contains(product2, allProducts);
+            Assert.Contains(product3, allProducts);
+        }
+
+        [Test]
+        public void UpdateProduct_ShouldUpdateProduct()
+        {
+            // Arrange
+            var existingProduct = new Product("Existing Product", 10.99m, "Existing Description", "Existing Supplier", DateTime.Now.AddDays(30));
+            _productManager.RegisterNewProduct(existingProduct);
+
+            // Act
+            _productManager.UpdateProduct("Existing Product", 15.99m, "Updated Description", "Updated Supplier", DateTime.Now.AddDays(60));
+
+            // Assert
+            var updatedProduct = _productManager.GetProductByName("Existing Product");
+            Assert.AreEqual(15.99m, updatedProduct.Price);
+            Assert.AreEqual("Updated Description", updatedProduct.Description);
+            Assert.AreEqual("Updated Supplier", updatedProduct.Supplier);
+            Assert.AreEqual(DateTime.Now.AddDays(60).Date, updatedProduct.ExpiryDate.Date);
+        }
+
+        [Test]
+        public void DeleteProduct_ShouldDeleteProduct()
+        {
+            // Arrange
+            var existingProduct = new Product("Product to Delete", 10.99m, "Description", "Supplier", DateTime.Now.AddDays(30));
+            _productManager.RegisterNewProduct(existingProduct);
+
+            // Act
+            _productManager.DeleteProduct("Product to Delete");
+
+            // Assert
+            var deletedProduct = _productManager.GetProductByName("Product to Delete");
+            Assert.IsNull(deletedProduct);
+        }
+    }
+    */
+}
